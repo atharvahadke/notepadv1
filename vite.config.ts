29f737b1bcfe94542b33aa9aa3756b4aa461d6da
Atharva <169069337+atharvahadke@users.noreply.github.com>
2026-02-08 15:45:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/',   // Required for Netlify
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '')
 
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-  },
+  return {
+    base: "./",   // ⭐ fixes blank site on Netlify
+    plugins: [react()],
 
-  plugins: [react()],
-
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
+    define: {
+      __GEMINI_API_KEY__: JSON.stringify(env.VITE_GEMINI_API_KEY)
     },
-  },
 
-  build: {
-    outDir: 'dist',
-  },
-});
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      }
+    }
+  }
+})
